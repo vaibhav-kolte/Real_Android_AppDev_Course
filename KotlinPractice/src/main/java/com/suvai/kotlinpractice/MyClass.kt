7fc7de5c4d1f8783c929431758051e0764d9fb99
vaibhav0910 <vaibhavkolte95@gmail.com>
2023-09-13 21:08:21 +0530

    Initial Commit

---
+++ KotlinPractice/src/main/java/com/suvai/kotlinpractice/MyClass.kt
@@ -0,0 +1,209 @@
+package com.suvai.kotlinpractice
+
+
+import kotlin.io.path.fileVisitor
+
+fun main() {
+
+    kotlinFundamental()
+    kotlinDatatypeDeclaration()
+    nullableTypeInKotlin()
+    kotlinNullProblemSolution()
+    kotlinElvisOperator()
+    kotlinComment()
+    ifCondition()
+    whenCondition()
+
+}
+
+fun whenCondition() {
+    val rating: Int = 4
+
+    val message = when (rating) {
+        1 -> "BAD ! RATING = $rating stars"
+        2 -> "NOT GOOD ! RATING = $rating stars"
+        3 -> "NOT BAD ! RATING = $rating stars"
+        4 -> "GOOD ! RATING = $rating stars"
+        5 -> "Perfect !!! Rating = $rating starts"
+        else -> "Nothing"
+    }
+
+    println(message)
+
+}
+
+fun ifCondition() {
+    println("\n\n\nTime")
+    val time = 10
+    val isAMOrPM = if (time < 12) {
+        "am"
+    } else {
+        "pm"
+    }
+    println(isAMOrPM)
+
+
+    val weight = 50
+    val myInstruction = "Put bag in ${if (weight < 50) "hold" else "cabin"}"
+    println(myInstruction)
+
+    val isComingOverBridge = false
+    var shootThem: Boolean
+    var holdPosition: Boolean
+    var blowTheBridge: Boolean
+
+    val wavingWhiteFlag = true
+
+    var friendlySoldiers = 2000
+    var enemySoldiers = 3000
+
+    if (wavingWhiteFlag) {
+        if (isComingOverBridge && friendlySoldiers > enemySoldiers) {
+            shootThem = true
+        } else if (isComingOverBridge && friendlySoldiers < enemySoldiers) {
+            blowTheBridge = true
+        } else {
+            shootThem = false
+            holdPosition = true
+        }
+    }
+}
+
+
+fun kotlinComment() {
+    // This is single line comment
+
+    /*
+    This is multi-line
+    comment
+     */
+}
+
+fun kotlinElvisOperator() {
+    print("My name is ${getMyName()}")
+
+}
+
+fun getMyName(): String {
+    var name: String? = "Vaibhav"
+
+//    if(name != null){
+//        return name
+//    }else{
+//        return "Android Studio"
+//    }
+
+
+    name = null
+
+    if (name != null) {
+        return name
+    } else {
+        return "Android Studio"
+    }
+
+
+//    return name ?: "Android Studio"
+}
+
+fun kotlinNullProblemSolution() {
+    val myName: String? = null
+
+    val uppercaseMyName = myName?.uppercase()
+//    val upperCase = myName!!.uppercase() // Exception
+
+//    if(myName != null){
+//        val uppercaseMyName = myName.uppercase()
+//    }
+    println("My Name $uppercaseMyName")
+
+    val firstNumber = 10
+    val secondNumber = 20
+    val result = firstNumber.times(secondNumber)
+
+    println("First Method $result")
+
+    val firstNumber1 = 10
+    val secondNumber1: Int? = 20
+    if (secondNumber1 != null) {
+        val result1 = firstNumber1.times(secondNumber1)
+        println("Second Method $result1")
+    }
+
+
+}
+
+fun nullableTypeInKotlin() {
+//    val userName: String = null // Error
+
+    val userName: String? = null
+    if (userName != null) {
+        val firstName = "Vaibhav"
+    }
+}
+
+fun kotlinDatatypeDeclaration() {
+    var myExample = "My little string"
+    myExample = myExample.uppercase()
+    println(myExample)
+
+    val userCount: Int = 10
+    val name = "Vaibhav" // STRING
+    val amount = 2.343 // DOUBLE
+}
+
+fun kotlinFundamental() {
+    println("Hello")
+
+    val yearOfBirth = 1996  // Immutable
+    val pi = 83.14f         // Immutable
+    var currentYear = 2024     // Mutable
+    var isItRaining = false     // Mutable
+
+
+    // TODO var variable can be reassign
+    currentYear = 2023
+    isItRaining = true
+
+
+    var ourProgrammingLanguage: String = "Kotlin"
+
+//    ourProgrammingLanguage = 11 // Error
+
+    // Assignment operator =
+    var greeting = "Hello"
+
+    // Addition operator +
+    var val1 = 10f
+    var val2 = 20
+    var addition = val1 + val2
+    println(addition)
+
+
+    // Subtraction operator
+    var myAge = currentYear - yearOfBirth
+    println("My age $myAge")
+
+    // Division operator
+    val div = val1 / val2
+    println("Division $div")
+
+
+    // Multiplication operator
+    val multi = val1 * val2
+    println("Multiplication $multi")
+
+
+    // Increment operator ++
+
+    val myAgeInNextYear = ++myAge // Pre-Increment first increment then assign
+    // myAge++ post-increment first assign then increment
+    println("My age in next year $myAgeInNextYear")
+
+    myAge--
+
+    // Decrement operator --
+    val myAgeInPreviousYear = --myAge // Pre-Decrement first decrement then assign
+    // myAge-- post-decrement first assign then decrement
+    println("My age in previous year $myAgeInPreviousYear")
+}
---
+++ app/src/main/java/com/suvai/realandroidappdevcourse/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.suvai.realandroidappdevcourse
+
+import android.app.KeyguardManager
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import javax.crypto.KeyGenerator
+
+
+
+//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
+class MainActivity : AppCompatActivity() {
+
+    // Kotlin Null Problem Solution
+    private lateinit var myName: String
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val txtMessage = findViewById<TextView>(R.id.txtMessage)
+        val name = "Vaibhav"
+        val yearOfBirth = 1996
+        var currentYear = 2023
+
+        var age: Int = currentYear - yearOfBirth
+
+        txtMessage.text = "$name is $age year old."
+
+
+
+
+        // Kotlin Null Problem Solution
+//        Log.e(Companion.TAG, "onCreate: $myName")  // If use before initialization got exception
+        myName = "Android Studio"
+        Log.e(Companion.TAG, "onCreate: $myName")
+
+
+
+        // Kotlin Type casting
+        val keyMgr = getSystemService(Context.KEYGUARD_SERVICE) as? KeyguardManager
+        if(keyMgr is KeyguardManager){
+            // do something
+        }
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+}
